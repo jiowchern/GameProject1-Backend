b6373fe1b445768ae3d83abd2d359bef91cecf2e
jc <jc@gmail.com>
2016-02-23 23:45:25 +0800

    邏輯資料抽出

--- Data/EntityData.cs
+++ Data/EntityData.cs
@@ -14,6 +14,23 @@ namespace Regulus.Project.ItIsNotAGame1.Data
         public ENTITY Name;
         [ProtoMember(2)]
         public Polygon Mesh;
-        
+
+
+        public static bool IsActor(ENTITY e)
+        {
+            return !IsWall(e);
+        }
+
+        public static bool IsWall(ENTITY e)
+        {
+            return e != Data.ENTITY.ACTOR1
+                   && e != Data.ENTITY.ENTRANCE
+                   && e != Data.ENTITY.ACTOR2
+                   && e != Data.ENTITY.ACTOR3
+                   && e != Data.ENTITY.ACTOR4
+                   && e != Data.ENTITY.ACTOR5
+                   && e != Data.ENTITY.DEBIRS;
+        }
+
     }
 }
