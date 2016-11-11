b03bc07a53d277f29949ce8e4330ddf86e722ea6
jc <jiowchern@yahoo.com.tw>
2016-11-11 16:50:58 +0800

    temp upload

--- Data/EntityGroupLayout.cs
+++ Data/EntityGroupLayout.cs
@@ -7,10 +7,25 @@ namespace Regulus.Project.ItIsNotAGame1.Data
 {
     [Serializable]
     public class EntityGroupLayout
-    {
-        
+    {        
         public EntityLayout[] Entitys;
-
+        public ChestLayout[] Chests;
+        public StaticLayout[] Statics;
         public string Id;
+    }
+
+    [Serializable]
+    public class StaticLayout
+    {
+        public Guid Owner;
+    }
+
+    [Serializable]
+    public class ChestLayout
+    {
+        public Guid Owner;
+        public Guid Exit;
+        public Guid Debirs;
+        public Guid Gate;
     }
 }
--- Data/EntityLayout.cs
+++ Data/EntityLayout.cs
@@ -4,15 +4,18 @@ using Regulus.CustomType;
 
 namespace Regulus.Project.ItIsNotAGame1.Data
 {
+    
+
     [Serializable]
-    public abstract class EntityLayout
+    public class EntityLayout
     {
+        public Guid Id;
         public ENTITY Type;
         public Vector2 Position;
-
         public float Direction;
-
-
-        public abstract EntityData Build();
     }
+
+    
+
+
 }
